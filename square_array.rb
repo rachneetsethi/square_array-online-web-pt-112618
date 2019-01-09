608abbe2ec9c68c167a2a19d5cc5145c2661f79f
Rachneet <rachneetsethi@gmail.com>
2019-01-09 18:40:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,8 @@ require "pry"
 def square_array(array)
   binding.pry 
   array = [1,2,3]
-  array.each do {|1| 1**2}
-  puts "here i have squared#{array} the number
+  array.each do |i| 
+    1**2
+  puts "here i have squared #{array} the number"
   end
 end
